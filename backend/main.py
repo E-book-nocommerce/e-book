aa6ecce3161e086f3386bc71e985f63b7487c29e
zvarych-oleksii <oleksii.zvarych@lnu.edu.ua>
2024-08-22 14:23:34 +0300

    mmm

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,24 @@ def read_root():
     return {"message": res}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
