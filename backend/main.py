1205026d37354b3145cb9da7ec6840ec7e144d3b
zvarych-oleksii <oleksii.zvarych@lnu.edu.ua>
2024-08-22 14:38:41 +0300

    fixed-version

--- backend/main.py
+++ backend/main.py
@@ -6,41 +6,9 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     res = 'Hello world!'
-    return {"message": res}
+    return {'message': res}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/items/{item_id}')
+def read_item(item_id: int, stroka: str = None):
+    return {'item_id': item_id, 'q': stroka}
