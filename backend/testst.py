547bfc7be0c1b80773dd2f0402c4a810a4ab402f
zvarych-oleksii <oleksii.zvarych@lnu.edu.ua>
2024-08-22 14:29:40 +0300

    men

---
+++ backend/testst.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    res = 'Hello world!'
+    return {"message": res}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
